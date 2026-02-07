f40b5f89ce50fdb8499627d1a176c5baddcac024
vasudev <vasujoshi2003@gmail.com>
2026-02-07 12:55:01 +0530

    bug fix

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const dashboardRoutes = require('./routes/dashboardRoutes');
 // Middleware
 app.use(cors({
   origin: [
-    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5500/index.html",
     "http://localhost:5500",
     "https://omverro.vercel.app",
     "https://https://omverro.vercel.app/index.html#"
